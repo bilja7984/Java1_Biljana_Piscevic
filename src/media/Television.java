39f78d3ad98fbd507fcc1ef68223e7447e19c3ff
Biljana <qa-prepodne@gmail.com>
2018-06-06 10:49:25 +0200

    Added new project Java1 and class Television with atributes and get and set methodes

---
+++ src/media/Television.java
@@ -0,0 +1,44 @@
+
+package media;
+
+public class Television {
+    
+    private int volume;
+    private int currentProgram;
+    private boolean turnOn;
+    
+    public Television() {
+        this.currentProgram = 0;
+    }
+    
+    public Television(int volume, int currentProgram, boolean turnOn) {
+        this.volume = volume;
+        this.currentProgram = currentProgram;
+        this.turnOn = turnOn;        
+    }
+    
+    public int getVolume() {
+        return this.volume;
+    }
+    
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    
+     public int getCurrentProgram() {
+        return this.currentProgram;
+    }
+    
+    public void setCurrentProgram(int currentProgram) {
+        this.currentProgram = currentProgram;
+    }
+    
+     public boolean getTurnOn() {
+        return this.turnOn;
+    }
+    
+    public void setTurnOn(boolean turnOn) {
+        this.turnOn = turnOn;
+    }
+    
+}
